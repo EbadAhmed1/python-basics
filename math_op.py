eef3e3d8fffe4f9de5b9ad8ed1142b8c4e113994
Ebad Ahmed <ebadahmed20005@gmail.com>
2025-07-24 22:58:12 +0500

    Arithmetic Operations

---
+++ math_op.py
@@ -0,0 +1,38 @@
+import math
+
+
+radius = float(input('Enter the radius of a circle: '))
+circumference = 2 * math.pi * radius
+
+rad = float(input("Enter the radius of a circle: "))
+area = math.pi * pow(radius, 2)
+print(f"Area of circle is: {area}cm^2")
+
+print(math.pi)
+print(math.e)
+
+
+friends = 6
+
+# friends = friends + 1
+# friends += 1
+# friends = friends - 2
+# friends -= 2
+# friends = friends * 3
+# friends *= 3
+# friends = friends / 2
+# friends /= 2
+# friends = friends ** 2
+
+remainder = friends % 2
+print(remainder)
+
+x = 3.14
+y = -4
+z = 5
+
+result = round(x)
+res = abs(y)
+resp = pow(4,3)
+max(x,y,z)
+min(x,y,z)
