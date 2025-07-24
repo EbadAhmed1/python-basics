813e7c404c0281558ac0d743e0a9e4f14dd381e0
Ebad Ahmed <ebadahmed20005@gmail.com>
2025-07-24 21:43:19 +0500

    variables

---
+++ variables.py
@@ -0,0 +1,42 @@
+#learning basics
+
+print("Anything Relevant")
+print("What about it")
+
+#strings
+first_name = "anything"
+food = "pizza"
+email = "ebadahmed20005@gmail.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"your email is {email}")
+
+#Integers
+age = 25
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+#Float 
+price = 10.99
+gpa = 3.2
+
+print(f"The price is ${price}")
+print(f"Your gpa is: {gpa}")
+
+#Boolean
+
+is_student = True
+
+print(f"Are you a student?: {is_student}")
+
+for_sale = False
+
+if for_sale:
+    print("for sale")
+else:  
+    print("not for sale")
