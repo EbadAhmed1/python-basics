88e25b8c40d179d5494121d88e4c8e920ba9d5fd
Ebad Ahmed <ebadahmed20005@gmail.com>
2025-07-24 22:23:43 +0500

    user input

---
+++ userinput.py
@@ -0,0 +1,15 @@
+name = input("What is your name: ")
+age = int(input("How old are you?: "))
+
+#age = int(age)
+age = age + 1
+print(f"Hello {name}!")
+print(f"You are {age} years old")
+
+
+#ex
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+area = length * width
+print(f"The area is: {area}cm")
