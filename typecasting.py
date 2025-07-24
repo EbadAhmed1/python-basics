199759d5ecfca468b35bfe38a6686cd2c657fc9d
Ebad Ahmed <ebadahmed20005@gmail.com>
2025-07-24 22:05:11 +0500

    type casting

---
+++ typecasting.py
@@ -0,0 +1,21 @@
+name = "Ebad Ahmed"
+skip = ""
+age = 21
+gpa = 3.2
+is_student = True
+
+print(type(name))
+
+gpa = int(gpa)
+print(gpa)
+
+age = str(age)
+print(type(age))
+
+age += "2"
+print(age)
+
+name = bool(name)
+skip = bool(skip)
+print(name)
+print(skip)
