426c7d791f9a6fabc1ee01167c50ba7775bd6ead
Ebad Ahmed <ebadahmed20005@gmail.com>
2025-08-05 00:30:44 +0500

    intro to GUI

---
+++ GUI.py
@@ -0,0 +1,19 @@
+# PyQt5 intro
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Graphical User Interface")
+        self.setGeometry(0,0,500,640)
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main() 
